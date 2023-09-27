3300e3f064a8e2edb8ba3a7d02d3fd63bfb21733
Jeremy Oden <jerem.oden@gmail.com>
2023-09-27 21:43:57 +0200

    Add concat test.

--- tests.cpp
+++ tests.cpp
@@ -2,12 +2,16 @@
 
 int main()
 {
-    using list = type_list<int, float, double>;
+    using list2 = type_list<char, int>;
+    using concat_list = concat<list1, list2>::type;
 
     // size
     static_assert(size<list>::value == 3);
 
     // any_of
     constexpr auto is_int = []<typename T>(T) { return std::is_same_v<T, int>; };
-    static_assert(any_of<list, is_int>::type::value == true);
+    static_assert(any_of<list1, is_int>::type::value == true);
+
+    // concat
+    static_assert(std::is_same_v<concat_list, type_list<int, float, double, char, int>>);
 }
